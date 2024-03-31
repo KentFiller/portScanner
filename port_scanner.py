ebb0df63da6d3b2a188c7145f48f73f82746594a
KentFiller <118877748+KentFiller@users.noreply.github.com>
2024-03-31 14:09:03 +0800

    Create port_scanner.py

---
+++ port_scanner.py
@@ -0,0 +1,20 @@
+#! usr/bin/python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+# hackthissite.org ip: "137.74.187.104"
+# ports: 443 (https), 21 (ftp), 22 (ssh)
+
+host = input("Enter the IP you want to scan: ")
+port = int(input("Enter the Port you want to scan: "))
+
+def port_scanner(port):
+    if s.connect_ex((host, port)):
+        print("The Port is Closed")
+    else:
+        print("The Port is Open")
+
+port_scanner(port)
